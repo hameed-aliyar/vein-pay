6b727bf9c60e313355bd1cbda984a5eae4f8b76a
Abdul Hameed T A <hameedamithattuparambil@gmail.com>
2026-05-03 18:30:30 +0530

    host allowed.

--- vein_pay_frontend/src/api/api.js
+++ vein_pay_frontend/src/api/api.js
@@ -3,10 +3,6 @@ import axios from 'axios';
 // Create a new Axios instance with a custom configuration
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    "Cache-Control": "no-cache",
-    "Pragma": "no-cache"
-  }
 });
 
 // Use an interceptor to add the auth token to every request
